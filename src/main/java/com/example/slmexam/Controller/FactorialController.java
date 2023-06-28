9a2722a5e887f362b76e1c06a82eb30e4b5a4ae0
ali <wi22b064@technikum-wien.at>
2023-06-28 18:41:18 +0200

    added method add and 2 tests

--- src/main/java/com/example/slmexam/Controller/FactorialController.java
+++ src/main/java/com/example/slmexam/Controller/FactorialController.java
@@ -7,13 +7,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FactorialController {
 
+    int total = 0;
     @RequestMapping("/api/factorial")
     public int calculate(@RequestParam int number){
         int factorial = 1;
         for (int i = 2; i <= number; i++) {
             factorial = i * factorial;
         }
+        total += factorial;
         return factorial;
 
+    }
+
+    @RequestMapping("/api/factorial/total")
+    public int add(){
+        return total;
     }
 }
--- src/test/java/com/example/slmexam/Controller/FactorialControllerTest.java
+++ src/test/java/com/example/slmexam/Controller/FactorialControllerTest.java
@@ -12,4 +12,30 @@ public class FactorialControllerTest {
         int result = controller.calculate(5);
         assertEquals(result, 120);
     }
+
+    @Test
+    void addFactorial(){
+        FactorialController controller = new FactorialController();
+        int result = controller.add();
+        assertEquals(result, 0);
+    }
+
+    @Test
+    void addFactorialFlowTest(){
+        FactorialController controller = new FactorialController();
+        //print total
+        int result = controller.add();
+        assertEquals(result, 0);
+        //calculate
+        result = controller.calculate(5);
+        assertEquals(result, 120);
+        //calculate
+        result = controller.calculate(4);
+        assertEquals(result, 24);
+        //print total
+        result = controller.add();
+        assertEquals(result, 144);
+    }
+
+
 }
